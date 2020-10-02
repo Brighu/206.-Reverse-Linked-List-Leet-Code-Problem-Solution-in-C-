7aa494377f9ab7ce752e7889f8c6eb342fa3dc3d
Brighu-Raina <72229896+Brighu-Raina@users.noreply.github.com>
2020-10-02 13:44:37 +0530

    Create Reverse.cpp

---
+++ Reverse.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head==nullptr)
+            return head;
+        stack<ListNode*> s1;
+        ListNode* temp;
+        temp=head;
+        while(temp!=nullptr){
+            s1.push(temp);
+            temp=temp->next;
+        }
+        temp=s1.top();
+        head=temp;
+        s1.pop();
+        while(!s1.empty()){
+            temp->next=s1.top();
+            s1.pop();
+            temp=temp->next;
+        }
+        temp->next=nullptr;
+        temp=nullptr;
+        free(temp);
+        return head;
+    }
+};
